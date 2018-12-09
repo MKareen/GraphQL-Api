b9ea00c1e0afbae1a730314f982b2f4e4827c47e
Karen Margaryan <karen.margaryan.2015@gmail.com>
2018-12-09 18:15:39 +0400

    comments in scalars

--- src/graphql/scalars/Date.js
+++ src/graphql/scalars/Date.js
@@ -4,9 +4,9 @@ import { Kind } from 'graphql/language';
 export const GraphQLDate = new GraphQLScalarType({
     name: 'DateType',
     description: 'Date custom scalar type',
-    serialize: (value) => value.getTime(),
-    parseValue: (value) => new Date(value),
-    parseLiteral: (ast) => {
+    serialize: (value) => value.getTime(), // value sent to the client
+    parseValue: (value) => new Date(value), // value from the client
+    parseLiteral: (ast) => { // ast value is always in string format
         if (ast.kind === Kind.INT) {
             return new Date(ast.value);
         }
